bcd2ca493647dc82336b4eb2f3b48ec28c5da5bb
Lilian BAZILLE <lilian@bazille.me>
2018-10-08 11:10:35 +0200

    Adding comments on config file.

--- src/config.php
+++ src/config.php
@@ -1,10 +1,14 @@
 <?php
 
-$routerA = "10.0.0.1";
-$routerB = "10.0.0.2";
-$lanSubnet = "10.0.0.0/24";
-$interface = "eth0";
-$localIP = "10.0.0.254";
+/*
+    All the equipements must be in the same LAN. 
+    The current machine must be the default gateway for the clients.
+*/
+$routerA = "10.0.0.1"; // xDSL router
+$routerB = "10.0.0.2"; // 4G router
+$lanSubnet = "10.0.0.0/24"; 
+$interface = "eth0"; // Name of the interface on this machine
+$localIP = "10.0.0.254"; // LAN IP address on the interface above
 
 const ROUTERA = 1;
 const ROUTERB = 2;
